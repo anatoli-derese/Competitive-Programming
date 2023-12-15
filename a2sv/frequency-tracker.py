9ad9d76f8679a0a769cc0d9c1846ae767e9dccba
anatoli-derese <135621823+anatoli-derese@users.noreply.github.com>
2023-12-15 22:35:00 +0300

    Added solution for Frequency Tracker

---
+++ a2sv/frequency-tracker.py
@@ -0,0 +1,25 @@
+class FrequencyTracker:
+    def __init__(self):
+        self.dict = defaultdict(int)
+        self.freq =  defaultdict(int)
+    
+    def add(self, number:int) -> None:
+        if self.freq[self.dict[number]] >0:
+            self.freq[self.dict[number]] -=1
+        self.dict[number] +=1
+        self.freq[self.dict[number]] +=1
+    def deleteOne (self,number:int) ->None:
+        if self.freq[self.dict[number]]  > 0:
+            self.freq[self.dict[number]]-=1
+        if self.dict[number] >=1:
+            self.dict[number] -=1
+        self.freq[self.dict[number]] +=1
+    def hasFrequency(self, frequency:int) ->bool:
+        return self.freq[frequency] > 0    
+
+
+# Your FrequencyTracker object will be instantiated and called as such:
+# obj = FrequencyTracker()
+# obj.add(number)
+# obj.deleteOne(number) frequency in self.d.values():
+# param_3 = obj.hasFrequency(frequency)
