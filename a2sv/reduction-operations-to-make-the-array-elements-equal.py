e8b61a110d28eb3fca77ceedcf28abb3a48af64b
anatoli-derese <135621823+anatoli-derese@users.noreply.github.com>
2023-12-29 18:04:32 +0300

    Added solution for Reduction Operations to Make the Array Elements Equal

--- a2sv/reduction-operations-to-make-the-array-elements-equal.py
+++ a2sv/reduction-operations-to-make-the-array-elements-equal.py
@@ -1,16 +1,11 @@
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
-        nums.sort()
-        count = Counter(nums)
-        i, j = len(nums)-2 , len(nums)-1
         ans = 0
-        while i >= 0:
-            if nums[i] != nums[j]:
-                ans += count[nums[j]]
-                count[nums[i]] += count[nums[j]]
-            i -=1
-            j -=1
-        return ans
+        nums.sort(reverse=True)
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i+1]:
+                ans += i+1
+        return ans    
 
 
 
