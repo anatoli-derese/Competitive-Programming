d7ab9d3e90f86acb751c3d41bd5eabe6d7363d32
anatoli-derese <135621823+anatoli-derese@users.noreply.github.com>
2024-01-01 19:44:29 +0300

    Added solution for K Closest Points to Origin

---
+++ a2sv/k-closest-points-to-origin.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points.sort(key = lambda x: sqrt(
+            x[0] **2 + x[1] **2
+        ))
+        ans = []
+        for i in range(k):
+            ans.append(points[i])
+        return ans
