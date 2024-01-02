8e2afceb4935966ff14bf75932fb4446112afe53
anatoli-derese <135621823+anatoli-derese@users.noreply.github.com>
2024-01-02 19:30:15 +0300

    Added solution for Valid Palindrome

--- a2sv/valid-palindrome.py
+++ a2sv/valid-palindrome.py
@@ -3,18 +3,14 @@ class Solution:
         s=s.lower()
         ans =""
         for letter in s:
-            
-            if (ord(letter) >= 97 and ord(letter)<=122) or (ord(letter)>= 48 and ord(letter)<=57) :
-                ans+= letter
-               
-        
-        back = ans[::-1]
-        
-        for i in range(len(ans)):
-            print(ans[i],back[i])
-            if ans[i] != back[i]:
-                
+            if letter.isalnum():
+                ans += letter
+        i,j = 0,len(ans)-1
+        while i < j:
+            if ans[i] != ans[j]:
                 return False
+            i +=1
+            j -=1
         return True
             
         
