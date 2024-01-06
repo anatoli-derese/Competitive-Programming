22104587ab3e1a96b0689669dab501cdbc8942bf
anatoli-derese <135621823+anatoli-derese@users.noreply.github.com>
2024-01-06 21:28:01 +0300

    Added solution for Long Pressed Name

---
+++ a2sv/long-pressed-name.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        name , typed = list (name), list(typed)
+        if name[0] != typed[0]: return False
+        i,j = 1,1 
+        while i < len(name) and j < len(typed):
+            if name[i] == typed[j]:
+                i += 1
+                j += 1
+            elif name[i-1] == typed[j]:
+                j +=1
+            else:
+                return False
+        while j < len(typed):
+            if name[i-1] == typed[j]:
+                j +=1
+            else:
+                return False
+        return i == len(name)
+
+        
