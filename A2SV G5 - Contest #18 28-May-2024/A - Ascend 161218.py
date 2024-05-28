52e94956c0ec671a4d70b2911e41f8a0ced91f41
anatoli-derese <135621823+anatoli-derese@users.noreply.github.com>
2024-05-28 19:45:51 +0300

    Created 'A - Ascend' by A2SV Hub

---
+++ A2SV G5 - Contest #18 28-May-2024/A - Ascend 161218.py
@@ -0,0 +1,12 @@
+# Problem: A - Ascend - https://codeforces.com/gym/526229/problem/A
+
+n =  int(input())
+lst = list(map(int,input().split()))
+ans = 1
+left = 0
+for right in range(1,n):
+    if lst[right] <= lst[right-1]:
+        left = right
+    wind = right- left +1
+    ans = max(wind, ans)
+print(ans)
